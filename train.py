3695677c960dd4870fb415b5ba089288e4f6c959
Pratit Shivaji Todkar <pratittodkar@gmail.com>
2021-06-04 18:27:20 +0530

    Reformatted train.py

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import onnx
 import onnxruntime as ort
 from onnx_tf.backend import prepare
 
+
 def area_of(left_top, right_bottom):
     """
     Compute the areas of rectangles given two corners.
@@ -20,6 +21,7 @@ def area_of(left_top, right_bottom):
     """
     hw = np.clip(right_bottom - left_top, 0.0, None)
     return hw[..., 0] * hw[..., 1]
+
 
 def iou_of(boxes0, boxes1, eps=1e-5):
     """
@@ -38,6 +40,7 @@ def iou_of(boxes0, boxes1, eps=1e-5):
     area0 = area_of(boxes0[..., :2], boxes0[..., 2:])
     area1 = area_of(boxes1[..., :2], boxes1[..., 2:])
     return overlap_area / (area0 + area1 - overlap_area + eps)
+
 
 def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
     """
@@ -72,6 +75,7 @@ def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
 
     return box_scores[picked, :]
 
+
 def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5, top_k=-1):
     """
     Select boxes that contain human faces
@@ -100,9 +104,9 @@ def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5
         subset_boxes = boxes[mask, :]
         box_probs = np.concatenate([subset_boxes, probs.reshape(-1, 1)], axis=1)
         box_probs = hard_nms(box_probs,
-           iou_threshold=iou_threshold,
-           top_k=top_k,
-           )
+                             iou_threshold=iou_threshold,
+                             top_k=top_k,
+                             )
         picked_box_probs.append(box_probs)
         picked_labels.extend([class_index] * box_probs.shape[0])
     if not picked_box_probs:
@@ -114,6 +118,7 @@ def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5
     picked_box_probs[:, 3] *= height
     return picked_box_probs[:, :4].astype(np.int32), np.array(picked_labels), picked_box_probs[:, 4]
 
+
 onnx_path = 'models/ultra_light/ultra_light_models/ultra_light_640.onnx'
 onnx_model = onnx.load(onnx_path)
 predictor = prepare(onnx_model)
@@ -122,7 +127,6 @@ input_name = ort_session.get_inputs()[0].name
 
 shape_predictor = dlib.shape_predictor('models/facial_landmarks/shape_predictor_5_face_landmarks.dat')
 fa = face_utils.facealigner.FaceAligner(shape_predictor, desiredFaceWidth=112, desiredLeftEye=(0.3, 0.3))
-
 
 TRAINING_BASE = 'faces/training/'
 
@@ -154,10 +158,10 @@ for label in dirs:
 
                 # if face detected
                 if boxes.shape[0] > 0:
-                    x1, y1, x2, y2 = boxes[0,:]
+                    x1, y1, x2, y2 = boxes[0, :]
                     gray = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
-                    aligned_face = fa.align(raw_img, gray, dlib.rectangle(left = x1, top=y1, right=x2, bottom=y2))
-                    aligned_face = cv2.resize(aligned_face, (112,112))
+                    aligned_face = fa.align(raw_img, gray, dlib.rectangle(left=x1, top=y1, right=x2, bottom=y2))
+                    aligned_face = cv2.resize(aligned_face, (112, 112))
 
                     cv2.imwrite(f'faces/tmp/{label}_{frame_count}.jpg', aligned_face)
 
@@ -180,7 +184,7 @@ with tf.Graph().as_default():
         embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
         phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
 
-        feed_dict = { images_placeholder: images, phase_train_placeholder:False }
+        feed_dict = {images_placeholder: images, phase_train_placeholder: False}
         embeds = sess.run(embeddings, feed_dict=feed_dict)
         with open("embeddings/embeddings.pkl", "wb") as f:
             pickle.dump((embeds, names), f)
